eec8280681f85d11855b44e9d1df41285b102579
JugBub <sebbbes2@gmail.com>
2020-08-11 19:06:32 +0200

    Dolphin buttons woork

--- src/com/jugbub/Window.java
+++ src/com/jugbub/Window.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class Window {
 
-    public static void window(ArrayList<File> games, ArrayList<Launch> buttons, ArrayList<Icon> icons){
+    public static void window(ArrayList<File> games, ArrayList<Launch> buttons, ArrayList<Icon> icons) throws FileNotFoundException {
         JFrame frame = new JFrame("Launcher");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JRootPane root = frame.getRootPane();
@@ -20,7 +20,7 @@ public class Window {
         buttons.forEach((n) -> frame.add(n.button));
         frame.setSize(750,450);
 
-        setupButtons(games, buttons, icons);
+        setupButtons(buttons, icons);
 
         frame.setLayout(null);
         frame.setVisible(true);
@@ -49,9 +49,13 @@ public class Window {
         return bar;
     }
 
-    private static void setupButtons(ArrayList<File> games,ArrayList<Launch> buttons, ArrayList<Icon> icons){
+    private static void setupButtons(ArrayList<Launch> buttons, ArrayList<Icon> icons) throws FileNotFoundException {
+        ArrayList<File> apps = GamePathsTXT.getAllFiles();
+
+
         for (int i = 0; i < buttons.size(); i++) {
             buttons.get(i).button.setBounds(95*i,0,95,95);
+            Launch.setApp(buttons.get(i), apps.get(i));
             //buttons.get(i).setIcon(icons.get(i));
             final int ITERATION = i;
             buttons.get(i).button.addActionListener(new ActionListener() {
@@ -59,10 +63,9 @@ public class Window {
                 public void actionPerformed(ActionEvent e){
                     try {
                         Runtime runtime = Runtime.getRuntime();
-                        String gamePath = buttons.get(ITERATION).application.getAbsolutePath();
-                        String[] strings = {"F:\\Downloads\\dolphin-master-5.0-8830-x64\\dolphin-master-5.0-9320-x64\\Dolphin-x64\\Dolphin.exe",gamePath} ;
+                        String[] strings = {"F:\\Downloads\\dolphin-master-5.0-8830-x64\\dolphin-master-5.0-9320-x64\\Dolphin-x64\\Dolphin.exe",buttons.get(ITERATION).application.getAbsolutePath()} ;
 
-                        System.out.println(gamePath);
+                        System.out.println(buttons.get(ITERATION).application.getAbsolutePath());
                         Process process = runtime.exec(/*String.format("%s %s",game.getPath() , games.get(ITERATION).getPath())*/strings);
                     } catch (IOException ioException) {
                         ioException.printStackTrace();
